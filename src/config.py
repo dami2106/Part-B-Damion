3a188cdbddf21c57d212a5ad3c94bacb003670e9
Dami <2303289@students.wits.ac.za>
2025-11-30 17:37:48 +0200

    Update requirements and warehouse config

--- src/config.py
+++ src/config.py
@@ -16,8 +16,8 @@ PEAK_HOURS = [8, 17]  # Peak hours during the day
 PEAK_MULTIPLIER = 2.0  # How much busier at peak times
 
 # Simulation parameters
-WAREHOUSE_WIDTH = 30
-WAREHOUSE_HEIGHT = 30
+WAREHOUSE_WIDTH = 20
+WAREHOUSE_HEIGHT = 20
 NUM_ROBOTS = 10
 SIMULATION_STEPS = None  # Will be calculated as N_DAYS * 24 * STEPS_PER_HOUR
 PRINT_INTERVAL = 20  # Print metrics every N steps
